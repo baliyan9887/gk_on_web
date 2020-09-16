2adf16ad4b9ccc1de628366377675e52e94c9909
baliyan9887 <gk150899@gmail.com>
2020-09-16 13:20:54 +0530

    added info sections

--- src/components/PageHeader/Header.js
+++ src/components/PageHeader/Header.js
@@ -13,16 +13,39 @@ import {
     CardBody,
     Nav,
     NavItem,
-    NavLink
+    NavLink,
+    UncontrolledCarousel,
+    CardTitle
 
 } from "reactstrap";
 
 // javascript plugin used to create scrollbars on windows
 import PerfectScrollbar from "perfect-scrollbar";
 
-// import Mike from '../../assets/'
 
 let ps = null;
+
+// CrouselItems
+
+const carouselItems = [
+  {
+    src: require("../../assets/img/denys.jpg"),
+    altText: "Slide 1",
+    caption: "Big City Life, United States"
+  },
+  {
+    src: require("../../assets/img/fabien-bazanegue.jpg"),
+    altText: "Slide 2",
+    caption: "Somewhere Beyond, United States"
+  },
+  {
+    src: require("../../assets/img/mark-finn.jpg"),
+    altText: "Slide 3",
+    caption: "Stocks, United States"
+  }
+];
+
+
 
 class Header extends React.Component {
 
@@ -61,6 +84,7 @@ constructor(props) {
 
   render() {
     return (
+      <>
       <div className="page-header header-filter">
         <div className="squares square1" />
         <div className="squares square2" />
@@ -177,6 +201,191 @@ constructor(props) {
           </Row>
         </Container>
       </div>
+      <div className="section">
+        {/* <img
+          alt="..."
+          className="dots"
+          src={require("../../assets/img/dots.png")}
+        />
+        <img
+          alt="..."
+          className="path"
+          style={{maxWidth: "65% !important"}}
+          src={require("../../assets/img/path4.png")}
+        /> */}
+        <Container>
+          <Row className="justify-content-between">
+            <Col md="6">
+              <Row className="justify-content-between align-items-center">
+                <UncontrolledCarousel items={carouselItems} />
+              </Row>
+            </Col>
+            <Col md="5">
+              <h1 className="profile-title text-left">Projects</h1>
+              <h5 className="text-on-back">02</h5>
+              <p className="profile-description text-left">
+                An artist of considerable range, Ryan — the name taken by
+                Melbourne-raised, Brooklyn-based Nick Murphy — writes,
+                performs and records all of his own music, giving it a warm,
+                intimate feel with a solid groove structure. An artist of
+                considerable range.
+              </p>
+              <div className="btn-wrapper pt-3">
+                <Button
+                  className="btn-simple"
+                  color="primary"
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
+                  <i className="tim-icons icon-book-bookmark" /> Bookmark
+                </Button>
+                <Button
+                  className="btn-simple"
+                  color="info"
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
+                  <i className="tim-icons icon-bulb-63" /> Check it!
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+
+      </div>
+      <section className="section section-lg">
+            <section className="section">
+              <img
+                alt="..."
+                className="path gksaw2"
+                src={require("../../assets/img/path4.png")}
+              />
+              <Container>
+                <Row className="row-grid justify-content-between">
+                  <Col className="mt-lg-5" md="5">
+                    <Row>
+                      <Col className="px-2 py-2" lg="6" sm="12">
+                        <Card className="card-stats">
+                          <CardBody>
+                            <Row>
+                              <Col md="4" xs="5">
+                                <div className="icon-big text-center icon-warning">
+                                  <i className="tim-icons icon-trophy text-warning" />
+                                </div>
+                              </Col>
+                              <Col md="8" xs="7">
+                                <div className="numbers">
+                                  <CardTitle tag="p">3,237</CardTitle>
+                                  <p />
+                                  <p className="card-category">Awards</p>
+                                </div>
+                              </Col>
+                            </Row>
+                          </CardBody>
+                        </Card>
+                      </Col>
+                      <Col className="px-2 py-2" lg="6" sm="12">
+                        <Card className="card-stats upper bg-default">
+                          <CardBody>
+                            <Row>
+                              <Col md="4" xs="5">
+                                <div className="icon-big text-center icon-warning">
+                                  <i className="tim-icons icon-coins text-white" />
+                                </div>
+                              </Col>
+                              <Col md="8" xs="7">
+                                <div className="numbers">
+                                  <CardTitle tag="p">3,653</CardTitle>
+                                  <p />
+                                  <p className="card-category">Commits</p>
+                                </div>
+                              </Col>
+                            </Row>
+                          </CardBody>
+                        </Card>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="px-2 py-2" lg="6" sm="12">
+                        <Card className="card-stats">
+                          <CardBody>
+                            <Row>
+                              <Col md="4" xs="5">
+                                <div className="icon-big text-center icon-warning">
+                                  <i className="tim-icons icon-gift-2 text-info" />
+                                </div>
+                              </Col>
+                              <Col md="8" xs="7">
+                                <div className="numbers">
+                                  <CardTitle tag="p">593</CardTitle>
+                                  <p />
+                                  <p className="card-category">Presents</p>
+                                </div>
+                              </Col>
+                            </Row>
+                          </CardBody>
+                        </Card>
+                      </Col>
+                      <Col className="px-2 py-2" lg="6" sm="12">
+                        <Card className="card-stats">
+                          <CardBody>
+                            <Row>
+                              <Col md="4" xs="5">
+                                <div className="icon-big text-center icon-warning">
+                                  <i className="tim-icons icon-credit-card text-success" />
+                                </div>
+                              </Col>
+                              <Col md="8" xs="7">
+                                <div className="numbers">
+                                  <CardTitle tag="p">10,783</CardTitle>
+                                  <p />
+                                  <p className="card-category">Forks</p>
+                                </div>
+                              </Col>
+                            </Row>
+                          </CardBody>
+                        </Card>
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col></Col>
+                  <Col md="5">
+                    
+                    <h1 className="profile-title text-left">Large Achivements</h1>
+                    <h5 className="text-on-back">02</h5>
+                      {/* <h1>
+                        Large <br />
+                        Achivements
+                      </h1> */}
+                      <div >
+                      <p>
+                        I should be capable of drawing a single stroke at the
+                        present moment; and yet I feel that I never was a
+                        greater artist than now.
+                      </p>
+                      <br />
+                      <p>
+                        When, while the lovely valley teems with vapour around
+                        me, and the meridian sun strikes the upper surface of
+                        the impenetrable foliage of my trees, and but a few
+                        stray.
+                      </p>
+                      <br />
+                      <a
+                        className="font-weight-bold text-info mt-5"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                      >
+                        Show all{" "}
+                        <i className="tim-icons icon-minimal-right text-info" />
+                      </a>
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            </section>
+          </section>
+      </>
     );
   }
 }
